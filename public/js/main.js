b85a2747a99149c5f5135796c61d75224ad236b4
matheus.silva <tecmatheusg@hotmail.com>
2023-01-17 14:59:02 -0300

    Cronômetro funcional

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,28 @@ var tamanhoFrase = $('#tamanho-frase')
 tamanhoFrase.text(numPalavras)
 
 var campo = $(".campo-digitacao")
-campo.on('click', function (){
+campo.on('input', function () {
     var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(' ').length
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1
+
     var contadorPalavras = $('#contador-palavras')
     contadorPalavras.text(qtdPalavras)
+
+    var qtdCaracteres = conteudo.length
+    $('#contador-caracteres').text(qtdCaracteres)
+})
+
+var tempoRestante = $('#tempo-digitacao').text()
+
+campo.one('focus', function () {
+    var cronometroId = setInterval(() => {
+        tempoRestante--
+        $('#tempo-digitacao').text(tempoRestante)
+
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true)
+            clearInterval(cronometroId)
+        }
+    }, 1000);
 })
